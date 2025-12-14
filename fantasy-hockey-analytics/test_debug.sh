f3d9224a1296300dc29612957b4137cc5cc1bac9
guenthal <guenthera13@gmail.com>
2025-12-14 13:24:01 -0500

    commit 4 -- cleanup and analysis display

--- fantasy-hockey-analytics/test_debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "n4r3jsf" | ./venv/bin/python main.py > debug_run.txt 2>&1
-echo "Debug output written to debug_run.txt"
-cat debug_run.txt | grep -A 20 "EXTRACTING STATS" | head -50
